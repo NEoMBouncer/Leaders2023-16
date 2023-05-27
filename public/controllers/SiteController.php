1e89b14a2de030ac799b1a374449ed4fb05aad7a
Jy4apa <xjyx10@mail.ru>
2023-05-27 12:55:39 +0300

    fix swagger

--- public/controllers/SiteController.php
+++ public/controllers/SiteController.php
@@ -1,7 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace public\controllers;
 
+use Yii;
+use yii\helpers\Url;
 use yii\web\Controller;
 
 /**
@@ -10,12 +12,31 @@ use yii\web\Controller;
 class SiteController extends Controller
 {
 
+
     /**
-     * @return string
+     * @inheritdoc
      */
+    public function actions(): array
+    {
+        return [
+            'docs' => [
+                'class' => \yii2mod\swagger\SwaggerUIRenderer::class,
+                'restUrl' => Url::to(['site/json-schema']),
+            ],
+            'json-schema' => [
+                'class' => \yii2mod\swagger\OpenAPIRenderer::class,
+                // Тhe list of directories that contains the swagger annotations.
+                'scanDir' => [
+                    Yii::getAlias('@api/modules/v1/controllers'),
+                    Yii::getAlias('@api/modules/v1/models'),
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
-        return render('/layouts/main.php');
+        return $this->redirect(['site/docs']);
     }
 
 }
