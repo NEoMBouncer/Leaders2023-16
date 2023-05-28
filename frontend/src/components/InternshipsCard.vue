2a86306019fc792d41957ba216b346a472289353
Igor Zhuravlev <pcqb10bouncer@gmail.com>
2023-05-28 20:35:10 +0300

    feat(app): intnerships

--- frontend/src/components/InternshipsCard.vue
+++ frontend/src/components/InternshipsCard.vue
@@ -2,13 +2,13 @@
   <div v-if="value">
     <div class="px-4 py-6 sm:px-6 lg:grid lg:grid-cols-12 lg:gap-x-6">
       <div class="sm:flex lg:col-span-7">
-        <router-link :to="value.href" class="aspect-h-1 aspect-w-1 w-full flex-shrink-0 overflow-hidden rounded-lg sm:aspect-none sm:h-40 sm:w-40">
+        <router-link :to="`/cabinet/internships/${value.id}`" class="aspect-h-1 aspect-w-1 w-full flex-shrink-0 overflow-hidden rounded-lg sm:aspect-none sm:h-40 sm:w-40">
           <img src="../assets/images/logo.png" alt="logo" class="h-full w-full object-contain object-center sm:h-full sm:w-full" />
         </router-link>
 
         <div class="mt-6 sm:ml-6 sm:mt-0">
           <h3 class="text-base font-medium text-gray-900">
-            <router-link :to="value.href">{{ value.name }}</router-link>
+            <router-link :to="`/cabinet/internships/${value.id}`">{{ value.title }}</router-link>
           </h3>
           <div class="flex mt-2">
             <div class="mr-2 flex items-center">
@@ -16,8 +16,8 @@
             </div>
             <p class="text-sm font-medium text-gray-900">Компания</p>
           </div>
-          <p class="mt-2 text-sm text font-medium text-gray-900">От 20000 руб</p>
-          <p class="mt-1 text-sm font-medium text-gray-900">{{ value.direction }}</p>
+          <p class="mt-2 text-sm text font-medium text-gray-900">От {{ value.income }} руб</p>
+          <p class="mt-1 text-sm font-medium text-gray-900">{{ direction }}</p>
           <p class="mt-2 text-sm text-gray-500">{{ value.description }}</p>
         </div>
       </div>
@@ -27,9 +27,7 @@
           <div>
             <dt class="font-medium text-gray-900">Адрес стажировки</dt>
             <dd class="mt-3 text-gray-500">
-              <span class="block">{{ value.address[0] }}</span>
-              <span class="block">{{ value.address[1] }}</span>
-              <span class="block">{{ value.address[2] }}</span>
+              <span class="block">{{ value.address }}</span>
             </dd>
           </div>
           <div>
@@ -42,7 +40,7 @@
               />
 
               <div class="ml-3">
-                <p class="text-sm font-medium text-gray-900 mt-2 xl:mt-0">Иван Иванович</p>
+                <p class="text-sm font-medium text-gray-900 mt-2 xl:mt-0">Иван Смирнов</p>
                 <div class="mt-2 flex items-center">
                   <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating" :class="[4 > rating ? 'text-indigo-500' : 'text-gray-300', 'h-5 w-5 flex-shrink-0']" aria-hidden="true" />
                 </div>
@@ -71,26 +69,26 @@
     </div>
     <div v-if="isOrganization" class="border-t border-gray-200 px-4 py-6 sm:px-6">
       <div class="flex justify-between">
-<!--         <span class="w-fit inline-flex items-center gap-x-1.5 rounded-md bg-red-100 px-2 py-1 font-medium text-red-700">-->
-<!--            <svg class="h-2 w-2 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">-->
-<!--              <circle cx="3" cy="3" r="3" />-->
-<!--            </svg>-->
-<!--            Отклонена-->
-<!--         </span>-->
-<!--        <span class="w-fit inline-flex items-center gap-x-1.5 rounded-md bg-green-100 px-2 py-1 font-medium text-green-700">-->
-<!--            <svg class="h-2 w-2 fill-green-500" viewBox="0 0 6 6" aria-hidden="true">-->
-<!--              <circle cx="3" cy="3" r="3" />-->
-<!--            </svg>-->
-<!--            Опубликована-->
-<!--         </span>-->
-        <span class="w-fit inline-flex items-center gap-x-1.5 rounded-md bg-blue-100 text-sm px-2 py-1 font-medium text-blue-700">
+         <span v-if="value.status === 2" class="w-fit inline-flex items-center gap-x-1.5 rounded-md bg-red-100 px-2 py-1 font-medium text-red-700">
+            <svg class="h-2 w-2 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">
+              <circle cx="3" cy="3" r="3" />
+            </svg>
+            Отклонена
+         </span>
+        <span v-if="value.status === 1" class="w-fit inline-flex items-center gap-x-1.5 rounded-md bg-green-100 px-2 py-1 font-medium text-green-700">
+            <svg class="h-2 w-2 fill-green-500" viewBox="0 0 6 6" aria-hidden="true">
+              <circle cx="3" cy="3" r="3" />
+            </svg>
+            Опубликована
+         </span>
+        <span v-if="value.status === 0" class="w-fit inline-flex items-center gap-x-1.5 rounded-md bg-blue-100 text-sm px-2 py-1 font-medium text-blue-700">
             <svg class="h-2 w-2 fill-blue-500" viewBox="0 0 6 6" aria-hidden="true">
               <circle cx="3" cy="3" r="3" />
             </svg>
             На модерации
          </span>
         <div class="flex">
-          <div class="text-sm text-green-500 hover:text-green-600 cursor-pointer flex items-center mr-3">
+          <div @click="edit(value.id)" class="text-sm text-green-500 hover:text-green-600 cursor-pointer flex items-center mr-3">
             <PencilIcon class="h-4 w-4 flex-shrink-0 mr-2" aria-hidden="true" />
             <span class="hidden sm:flex">Редактировать</span>
           </div>
@@ -121,6 +119,19 @@ export default {
   },
   components: {
     StarIcon, PencilIcon, TrashIcon
+  },
+  computed: {
+    direction() {
+      if(this.value && this.value?.direction?.list && this.value?.direction.direction_id) {
+        return this.value?.direction?.list[+this.value?.direction?.direction_id || '1']
+      }
+      return ''
+    }
+  },
+  methods: {
+    edit(id) {
+      this.$router.push(`/cabinet/applications-organization/${id}`)
+    }
   },
   data() {
     return {
