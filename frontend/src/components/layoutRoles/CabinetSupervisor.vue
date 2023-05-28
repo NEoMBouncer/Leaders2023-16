0cbae5187ccf3bb327b736dfad39709524aca099
Igor Zhuravlev <pcqb10bouncer@gmail.com>
2023-05-28 11:05:29 +0300

    feat(app): cabinets roles

---
+++ frontend/src/components/layoutRoles/CabinetSupervisor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <template v-if="!loading">
+    </template>
+    <loading v-else/>
+  </div>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+import Loading from "@/components/Loading.vue";
+
+export default {
+  name: "CabinetSupervisor",
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  computed: {
+  },
+  methods: {
+    ...mapActions('cabinet', ['getInfo', 'getDirections', 'getCandidate', 'setCreateOrder', 'setNextStage']),
+  },
+  async mounted() {
+    this.loading = true
+    this.loading = false
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- frontend/src/pages/cabinet/Cabinet.vue
+++ frontend/src/pages/cabinet/Cabinet.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="flex flex-col">
     <h1 class="text-2xl font-semibold pb-5">Кабинет</h1>
-    <!--ROLE_CANDIDATE-->
-    <template v-if="info.role === 0">
+    <!--кандидат/стажер-->
+    <template v-if="info.role === 0 || info.role === 1">
       <cabinet-candidate/>
+    </template>
+    <!--куратор-->
+    <template v-if="info.role === 2">
+      <cabinet-supervisor/>
+    </template>
+    <!--наставник-->
+    <template v-if="info.role === 0">
+      <cabinet-mentor/>
+    </template>
+    <!--кадровый специалист-->
+    <template v-if="info.role === 0">
+      <cabinet-organization/>
     </template>
   </div>
 </template>
@@ -12,10 +24,16 @@
 
 import {mapState} from "vuex";
 import CabinetCandidate from "@/components/layoutRoles/CabinetCandidate.vue";
+import CabinetSupervisor from "@/components/layoutRoles/CabinetSupervisor.vue";
+import CabinetMentor from "@/components/layoutRoles/CabinetMentor.vue";
+import CabinetOrganization from "@/components/layoutRoles/CabinetOrganization.vue";
 
 export default {
   name: "Cabinet",
   components: {
+    CabinetOrganization,
+    CabinetMentor,
+    CabinetSupervisor,
     CabinetCandidate
   },
   data() {
